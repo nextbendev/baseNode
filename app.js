3edbeeb69575ced739e1324c79ac2281dd1df035
Ben Sommerville <bs@Bens-MacBook-Air.local>
2019-07-06 16:20:50 -0400

    Init commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express =  require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.get('/', function(req,res){
+    res.send('<h1> hello</h1>');
+});
+
+app.listen(3000);
+
+console.log('Server is runing on port 3000')
